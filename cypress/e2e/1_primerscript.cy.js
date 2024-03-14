9bec35e320f1ad0432e1ac6b8da0a01aaac79fda
Rose <rose@gmail.com>
2024-03-14 10:01:44 -0300

    Primeros scripts con Cypress (métodos y comandos)

---
+++ cypress/e2e/1_primerscript.cy.js
@@ -0,0 +1,36 @@
+describe('AutomationPractice', () => {
+    it('TEST-01: Carga de la página principal', () => {
+        // Abrir la página de prueba
+        cy.visit('http://www.automationpractice.pl/');
+
+        // Chequear que el objeto 'Cart' (carrito de compras) esté visible
+        cy.contains('Cart').should('be.visible');
+    });
+});
+
+describe('SauceDemo', () => {
+    it('TEST-02: Inicio de Sesión con credenciales válidas', () => {
+        // Abrir la página de prueba
+        cy.visit('https://www.saucedemo.com/');
+
+        // Ingresar el valor del usuario
+        cy.get('[data-test="username"]').type('standard_user');
+
+        // Ingresar el valor del contraseña
+        cy.get('[data-test="password"]').type('secret_sauce');
+
+        // Hacer clic en el botón Login
+        cy.get('[data-test="login-button"]').click();
+
+        // Hacer un chequeo del login exitoso
+        cy.get('.title').should('have.text', 'Products'); 
+        cy.get('.shopping_cart_link').should('exist');
+    });
+
+    it('TEST-03: Inicio de Sesión usando comandos', () => {
+        cy.visit('https://www.saucedemo.com/');
+
+        // Invocación al comando iniciarSesion
+        cy.iniciarSesion('standard_user', 'secret_sauce');
+    });
+});
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,18 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+Cypress.Commands.add('iniciarSesion', (usuario, password) => {
+     // Ingresar el valor del usuario
+     cy.get('[data-test="username"]').type(usuario);
+
+     // Ingresar el valor del contraseña
+     cy.get('[data-test="password"]').type(password);
+
+     // Hacer clic en el botón Login
+     cy.get('[data-test="login-button"]').click();
+
+     // Hacer un chequeo del login exitoso
+     cy.get('.title').should('have.text', 'Products'); 
+     cy.get('.shopping_cart_link').should('exist');
+});
